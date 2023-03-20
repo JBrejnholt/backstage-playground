85e7e238b1d0a51862011973b6cff4f2a9e0c37c
jinhong.brejnholt <jinhong.brejnholt@velux.com>
2023-03-20 20:08:50 +0100

    feat: add demo plugin.

--- demo/packages/app/src/App.tsx
+++ demo/packages/app/src/App.tsx
@@ -35,6 +35,7 @@ import { AppRouter, FlatRoutes } from '@backstage/core-app-api';
 import { CatalogGraphPage } from '@backstage/plugin-catalog-graph';
 import { RequirePermission } from '@backstage/plugin-permission-react';
 import { catalogEntityCreatePermission } from '@backstage/plugin-catalog-common/alpha';
+import { DemoPluginPage } from '@internal/plugin-demo-plugin';
 
 const app = createApp({
   apis,
@@ -107,6 +108,7 @@ const routes = (
     </Route>
     <Route path="/settings" element={<UserSettingsPage />} />
     <Route path="/catalog-graph" element={<CatalogGraphPage />} />
+    <Route path="/demo-plugin" element={<DemoPluginPage />} />
   </FlatRoutes>
 );
 
